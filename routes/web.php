12778f6705e7af683ffcf074f04ad713d37ac4ca
ivanopol <ivanopol777@mail.ru>
2020-06-03 15:19:07 +0500

    #clear Удалил лишние страницы

--- routes/web.php
+++ routes/web.php
@@ -13,22 +13,5 @@
 
 Route::get('send', 'FeedbackController@send');
 Route::middleware(['utm.check', 'cookie.check', 'counter'])->group(function () {
-    Route::get('/{city}/privacy', 'HomeController@privacy')->name('privacy');
-    Route::get('/{city}/stocks', 'HomeController@stocks')->name('stocks');
-    Route::get('/{city}/stocks/{stocks_title}', 'HomeController@stocks_details')->name('stocks.one');
-    Route::get('/{city}/news', 'HomeController@news')->name('news');
-    Route::get('/{city}/news/{news_title}', 'HomeController@news_details')->name('news.one');
     Route::get('/{city?}', 'HomeController@index')->name('index');
-    Route::get('/{city?}/special_offers', 'HomeController@special_offers')->name('special_offers');
-    Route::get('/{city?}/{car_model}/{car_type}', 'HomeController@model')->name('model');
-    Route::get('/{city?}/{car_model}/{car_type}/model_details', 'HomeController@model_details')->name('model_details');
-    Route::get('/alt_menu', 'HomeController@alt_menu')->name('alt_menu');
-    Route::get('/model_details', 'HomeController@model_details')->name('model_details');
-    Route::get('/{city?}/{car_model}/{car_type}/trade_in_calc', 'HomeController@trade_in_calc')->name('trade_in_calc');
-    Route::get('/main', 'HomeController@main')->name('main');
-    Route::get('/get_brands', 'HomeController@get_brands')->name('get_brands');
-    Route::any('/send_contact_form', 'ContactFormController@sendContactForm')->name('sendContactForm');
-    Route::any('/check_visitor_cookie', 'ContactFormController@checkVisitorCookie')->name('checkVisitorCookie');
-    Route::get('/{city}/contacts', 'HomeController@contacts')->name('contacts');
-    Route::get('/{city}/service', 'HomeController@service')->name('service');
 });
